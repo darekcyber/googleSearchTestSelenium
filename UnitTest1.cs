8646f2309b3c6a2523ce0cd31951a610ae23cbea
darekcyber <darrcyber@gmail.com>
2021-08-20 15:40:53 +0200

    Opening a website in Firefox

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,18 +1,32 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Threading;
 
 namespace googleTestSearch
 {
     public class Tests
     {
+        IWebDriver driver;
+
         [SetUp]
         public void Setup()
         {
+            driver = new FirefoxDriver();
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            driver.Url = "https://google.com";
+            Thread.Sleep(2000);
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Quit();
         }
     }
 }
